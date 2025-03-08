f4a741de962af62c3d809ba4e82eac159934152d
Ayşe Nisa Şen <aysenisasen471@icloud.com>
2025-03-08 17:55:27 +0300

    Add files via upload

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard"; // Yeni eklenen sayfa
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" />} /> {/* Geçersiz bir sayfa girildiğinde login'e yönlendirme */}
+      </Routes>
+    </Router>
   );
 }
 
