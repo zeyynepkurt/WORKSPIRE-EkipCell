3555a49e4c44a3207918472e8fc39a329c749901
Zeynep Kurt <109173044+zeyynepkurt@users.noreply.github.com>
2025-03-14 12:43:43 +0300

    Add files via upload

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard"; // Yeni eklenen sayfa
+import TaskCalendar from "./components/Calendar"; // Takvim sayfasını ekledik
+import TeamMembers from "./components/TeamMembers";
+import TeamMemberDetail from "./components/TeamMemberDetails";
+
 
 function App() {
   return (
@@ -8,7 +12,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Dashboard />} />
+        <Route path="/calendar" element={<TaskCalendar />} />  
         <Route path="*" element={<Navigate to="/" />} /> {/* Geçersiz bir sayfa girildiğinde login'e yönlendirme */}
+        <Route path="/team-members" element={<TeamMembers />} />
+        <Route path="/team-members/:id" element={<TeamMemberDetail />} />
+
       </Routes>
     </Router>
   );
