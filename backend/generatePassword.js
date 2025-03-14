9c248f4a4eac7e3a793f67b943060653b6f85f7e
Zeynep Kurt <109173044+zeyynepkurt@users.noreply.github.com>
2025-03-14 12:45:15 +0300

    Add files via upload

--- backend/generatePassword.js
+++ backend/generatePassword.js
@@ -1,9 +1,13 @@
 const bcrypt = require('bcrypt');
 
 const users = [
-  { employee_id: 1, password: "sifre1" },
-  { employee_id: 2, password: "sifre2" },
-  { employee_id: 3, password: "sifre123" }
+  //{ employee_id: 1, password: "sifre1" },
+  //{ employee_id: 2, password: "sifre2" },
+  //{ employee_id: 3, password: "sifre123" },
+  // { employee_id: 4, password: "sifre1234" },
+  // { employee_id: 5, password: "sifre12345" },
+  // { employee_id: 6, password: "sifre123456" }
+  { employee_id: 7, password: "sifre1234567" }
 ];
 
 async function hashPasswords(users) {
