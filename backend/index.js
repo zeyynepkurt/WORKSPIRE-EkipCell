362a89b4924f852e52924d32db542d56367bd7d5
Ayşe Nisa Şen <aysenisasen471@icloud.com>
2025-03-21 12:32:29 +0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
