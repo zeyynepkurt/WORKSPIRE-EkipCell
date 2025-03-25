13da6bf226e6e14a02a6fd0e4e5ded7b00f45ff9
Ayşe Nisa Şen <aysenisasen471@icloud.com>
2025-03-26 01:53:53 +0300

    Add files via upload

--- clients/src/App.js
+++ clients/src/App.js
@@ -5,20 +5,34 @@ import TaskCalendar from "./components/Calendar"; // Takvim sayfasını ekledik
 import TeamMembers from "./components/TeamMembers";
 import TeamMemberDetail from "./components/TeamMemberDetails";
 import PomodoroTimer from "./components/Pomodoro";
+import ManagerHome from './components/ManagerHome';
 
 
 function App() {
+  // Kullanıcı giriş yapmış mı kontrol et
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+  const employeeId = localStorage.getItem("employeeId"); // Kullanıcının ID'si
+  const managerId = localStorage.getItem("managerId");   // Kullanıcının Manager ID'si
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Dashboard />} />
-        <Route path="/calendar" element={<TaskCalendar />} />  
-        <Route path="*" element={<Navigate to="/" />} /> {/* Geçersiz bir sayfa girildiğinde login'e yönlendirme */}
-        <Route path="/team-members" element={<TeamMembers />} />
-        <Route path="/team-members/:id" element={<TeamMemberDetail />} />
-        <Route path="/pomodoro" element={<PomodoroTimer />} />
+        
+        {/* Kullanıcı giriş yapmışsa kendi Dashboard'una yönlendir */}
+        <Route path="/home" element={isAuthenticated ? <Dashboard employeeId={employeeId} /> : <Navigate to="/" />} />
 
+        {/* Yönetici (managerId = 1) ekranı */}
+        <Route path="/manager" element={isAuthenticated && managerId === "1" ? <ManagerHome /> : <Navigate to="/" />} />
+        
+        {/* Diğer sayfalar */}
+        <Route path="/calendar" element={isAuthenticated ? <TaskCalendar /> : <Navigate to="/" />} />  
+        <Route path="/team-members" element={isAuthenticated ? <TeamMembers /> : <Navigate to="/" />} />
+        <Route path="/team-members/:id" element={isAuthenticated ? <TeamMemberDetail /> : <Navigate to="/" />} />
+        <Route path="/pomodoro" element={isAuthenticated ? <PomodoroTimer /> : <Navigate to="/" />} />
+
+        {/* Geçersiz bir sayfa girildiğinde login'e yönlendirme */}
+        <Route path="*" element={<Navigate to="/" />} /> 
       </Routes>
     </Router>
   );
