8197062e6a70d59d7068825e00d8b154b6e6d8b5
Zeynep Kurt <109173044+zeyynepkurt@users.noreply.github.com>
2025-04-11 13:54:43 +0300

    Update Navbar.js

--- clients/src/components/Navbar.js
+++ clients/src/components/Navbar.js
@@ -1,11 +1,16 @@
 import { FaBars, FaBell, FaEnvelope, FaUserCircle, FaSearch, FaSun, FaMoon } from "react-icons/fa";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import ChatBox from "./ChatBox";
 
 const Navbar = ({ darkMode, setDarkMode, language, setLanguage, setMenuOpen }) => {
   const translations = {
     tr: { search: "Ara..." },
     en: { search: "Search..." }
   };
-
+  const [chatOpen, setChatOpen] = useState(false);
+  const navigate = useNavigate();
   return (
     <nav className={`top-4 left-2 right-2 flex justify-between items-center p-4 ${darkMode ? "bg-blue-800" : "bg-blue-900"} text-white shadow-md rounded-xl relative z-40`}>
       <div className="flex items-center gap-4">
@@ -22,7 +27,18 @@ const Navbar = ({ darkMode, setDarkMode, language, setLanguage, setMenuOpen }) =
         </div>
       </div>
       <div className="flex items-center gap-6">
-        <FaEnvelope className="text-2xl cursor-pointer" />
+        <FaEnvelope
+          className="text-2xl cursor-pointer"
+          onClick={() => navigate("/messages")}
+        />
+
+        {chatOpen && (
+          <div className="absolute right-4 top-16 md:right-8 z-50">
+            <ChatBox />
+          </div>
+        )}
+
+
         <FaBell className="text-2xl cursor-pointer" />
         <FaUserCircle className="text-3xl cursor-pointer" />
         <button onClick={() => setDarkMode(!darkMode)} className="text-xl">
