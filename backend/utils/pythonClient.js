586d32d953244724ded8b2a4f4dfd5644fd98610
Zeynep Kurt <109173044+zeyynepkurt@users.noreply.github.com>
2025-05-11 08:09:37 +0300

    Add files via upload

--- backend/utils/pythonClient.js
+++ backend/utils/pythonClient.js
@@ -1,1 +1,23 @@
+const axios = require("axios");
 
+async function getTaskPriorities(tasks) {
+  try {
+    const response = await axios.post("http://ml:5001/predict", { tasks });
+    console.log("🎯 AI'den gelen cevap:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("⚠️ AI servisine bağlanılamadı:", error.message);
+    if (error.response) {
+      console.error("📦 Response data:", error.response.data);
+      console.error("📦 Status:", error.response.status);
+      console.error("📦 Headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("📡 Request:", error.request);
+    } else {
+      console.error("❌ Error:", error.message);
+    }
+    return [];
+  }
+}
+
+module.exports = { getTaskPriorities };
